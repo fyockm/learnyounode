90a2daf0c7eb9a511c7beb416b6c9f24f3424177
Drew Fyock <fyockm@gmail.com>
2014-06-18 23:30:35 -0700

    ex12

--- ex12.js
+++ ex12.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var map = require('through2-map');
 
 var server = http.createServer(function(req, resp) {
+	if (req.method !== 'POST')
+		return res.end('not a POST\n');
 	resp.writeHead(200, {
 		'content-type': 'text/plain'
 	});
-	fs.createReadStream(process.argv[3]).pipe(resp);
+	req.pipe(map(function(chunk) {
+		return chunk.toString().toUpperCase();
+	})).pipe(resp);
 });
 server.listen(process.argv[2]);
